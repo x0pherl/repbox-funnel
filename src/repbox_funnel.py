9ffee4f52ee5299e8f9e84103a48557d41e5f11b
x0pherl <xopher@interiority.org>
2024-05-11 11:33:01 -0400

    migrated to multiple settings files and refactored funnes into a separate folder

--- src/repbox_funnel.py
+++ src/repbox_funnel.py
@@ -1,18 +1,20 @@
 """Module providing parts for a repbox filament funnel and external fitting."""
-from math import sqrt
 from configparser import ConfigParser
 from bd_warehouse.thread import TrapezoidalThread
-from build123d import (BuildPart, BuildSketch, Part, loft,
-                       Circle, Plane, RegularPolygon, fillet,
-                       vertices, extrude, chamfer, Text,
+from build123d import (BuildPart, BuildSketch, Part,
+                       Circle, RegularPolygon, fillet,
+                       vertices, extrude, chamfer, Text, Compound,
                        Mode, Location, Locations, revolve, CounterSinkHole,
                        Align, GeomType, SortBy, Axis, export_step, export_stl, add)
 from ocp_vscode import show
+from lib.funnels import hex_funnel
 
 REVISION_TEXT = "R1.0"
 
 config=ConfigParser()
-config.read('settings.ini')
+config.read('2mmIDx4mmOD-settings.ini')
+#config.read('settings.ini')
+#config.read('settings.ini')
 connector_depth = config.getfloat('connector','depth',fallback=6.5)
 connector_diameter = config.getfloat('connector', 'diameter', fallback=10.1)
 connector_pitch = config.getfloat('connector', 'pitch', fallback=0.874)
@@ -20,7 +22,6 @@ connector_pitch = config.getfloat('connector', 'pitch', fallback=0.874)
 shaft_length = config.getfloat('shaft', 'length', fallback=20)
 shaft_diameter = config.getfloat('shaft', 'diameter', fallback=11.5)
 shaft_interference = config.getfloat('shaft', 'interference', fallback=0.5)
-
 
 bend_angle = config.getfloat('bend', 'angle', fallback=10)
 
@@ -39,49 +40,6 @@ tube_outer_diameter = config.getfloat('tube', 'outer_diameter', fallback=6.5)
 tube_inner_diameter = config.getfloat('tube', 'inner_diameter', fallback=3.5)
 
 font_path = config.get('general', 'font-path', fallback='C:\\Windows\\Fonts\\arial.ttf')
-
-def cone_funnel(lower_radius=10, upper_radius=20, inner_radius=5, height=30, minimum_wall=0):
-    """Function generating a round funnel."""
-    with BuildPart() as outer_funnel:
-        with BuildSketch(Plane(origin=(0, 0,0), z_dir=(0, 0, 1))):
-            Circle(lower_radius)
-        with BuildSketch(Plane(origin=(0, 0,height), z_dir=(0, 0, 1))):
-            Circle(upper_radius)
-        loft()
-    with BuildPart() as inner_funnel:
-        with BuildSketch(Plane(origin=(0, 0,0), z_dir=(0, 0, 1))):
-            Circle(inner_radius)
-        with BuildSketch(Plane(origin=(0, 0,height), z_dir=(0, 0, 1))):
-            Circle(upper_radius-minimum_wall)
-        loft()
-    funnel = Part(outer_funnel.part - inner_funnel.part)
-    if minimum_wall > 0:
-        funnel = funnel.fillet(radius=minimum_wall/4, edge_list=funnel.edges().sort_by(Axis.Z)[-2:])
-    return funnel
-
-def hex_funnel(lower_radius=10, upper_radius=20, inner_radius=5, height=30, minimum_wall=0):
-    """Function generating a hexagonal funnel."""
-    with BuildPart() as outer_funnel:
-        with BuildSketch(Plane(origin=(0, 0,0), z_dir=(0, 0, 1))):
-            RegularPolygon(radius=lower_radius, side_count=6)
-            fillet(vertices(), radius=lower_radius/4)
-        with BuildSketch(Plane(origin=(0, 0,height), z_dir=(0, 0, 1))):
-            RegularPolygon(radius=upper_radius, side_count=6)
-            fillet(vertices(), radius=upper_radius/4)
-        loft()
-
-    with BuildPart() as inner_funnel:
-        with BuildSketch(Plane(origin=(0, 0,0), z_dir=(0, 0, 1))):
-            Circle(inner_radius)
-        with BuildSketch(Plane(origin=(0, 0,height), z_dir=(0, 0, 1))):
-            Circle((upper_radius/2*sqrt(3))-minimum_wall)
-        loft()
-    funnel = Part(outer_funnel.part - inner_funnel.part)
-    if minimum_wall > 0:
-        wall_edges = funnel.edges().sort_by(Axis.Z)[-2:]
-        max_fillet_radius = funnel.max_fillet(wall_edges,max_iterations=100)
-        funnel = funnel.fillet(radius=max_fillet_radius, edge_list=wall_edges)
-    return funnel
 
 def socket_base(chamfer_thread=True):
     """Function generating the socket base -- a hexagonal nut with a revision label."""
@@ -107,7 +65,7 @@ def socket_base(chamfer_thread=True):
                 align=(Align.CENTER, Align.CENTER)
                 )
         extrude(amount=-.4, mode=Mode.SUBTRACT)
-    return base_part.part
+    return Compound(label="base", children=[base_part.part])
 
 def bend():
     """Function generating the transitional bend with a path to bend the PTFE tube."""
@@ -118,7 +76,7 @@ def bend():
                 fillet(vertices(), radius=hex_diameter/8)
                 Circle(tube_outer_diameter/2, mode=Mode.SUBTRACT)
         revolve(axis=Axis.X, revolution_arc=bend_angle)
-    return bend_part.part.moved(Location((0,connector_diameter*-2,0)))
+    return Compound(label="base", children=[bend_part.part.moved(Location((0,connector_diameter*-2,0)))])
 
 def build_external_fitting():
     """Function generating the complete external fitting."""
@@ -177,7 +135,7 @@ def build_external_fitting():
             .sort_by(Axis.Z)[-1],
             length=chamfer_radius,
         )
-    return Part(children=[outer_fitting.part, fitting_nut_thread, shaft_thread])
+    return Part (children=[outer_fitting.part, fitting_nut_thread, shaft_thread])
 
 def build_internal_fitting():
     """Function generating the assembled internal filament funnel."""
@@ -206,7 +164,8 @@ def build_internal_fitting():
                 minimum_wall = 1.5,
                 )
             )
-    return Part(children=[inner_fitting.part, fitting_nut_thread])
+    return Compound(label="filament funnel", children=[inner_fitting.part, fitting_nut_thread])
+    #return Part(children=[inner_fitting.part, fitting_nut_thread])
 
 external_fitting = build_external_fitting()
 internal_fitting = build_internal_fitting()
